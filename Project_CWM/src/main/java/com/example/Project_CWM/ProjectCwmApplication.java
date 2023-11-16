ef146fa64359199973d99b9eb883452de89a3a82
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2023-11-16 11:23:48 +0900

    231116 mybatis

--- Project_CWM/src/main/java/com/example/Project_CWM/ProjectCwmApplication.java
+++ Project_CWM/src/main/java/com/example/Project_CWM/ProjectCwmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectCwmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectCwmApplication.class, args);
 	}
