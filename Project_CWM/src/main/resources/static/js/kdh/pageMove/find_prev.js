51cedc77ee5936fa8239eb1d2a31202ecc582450
coffeegoodzz <ejsvk9811@naver.com>
2023-11-22 18:53:41 +0900

    수정

--- Project_CWM/src/main/resources/static/js/kdh/pageMove/find_prev.js
+++ Project_CWM/src/main/resources/static/js/kdh/pageMove/find_prev.js
@@ -2,5 +2,5 @@ let prebtn = document.querySelector(".login-header h3 i");
 
 prebtn.addEventListener('click', () => {
     console.log("성공");
-    location.href = "../longin/signin.html";
+    location.href = "/login";
 });
